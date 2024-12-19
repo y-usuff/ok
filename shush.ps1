2c9757ad7cf9698418534465b19f6c676369c45a
yusuf <136318971+y-usuff@users.noreply.github.com>
2024-12-19 14:02:27 +0000

    Update shush.ps1

--- shush.ps1
+++ shush.ps1
@@ -64,25 +64,3 @@ $originalPOS = [System.Windows.Forms.Cursor]::Position.X
 $PlayWav=New-Object System.Media.SoundPlayer;$PlayWav.SoundLocation="$env:TMP\e.wav";$PlayWav.playsync()
 
 ############################################################################################################################################################
-
-<#
-
-.NOTES 
-	This is to clean up behind you and remove any evidence to prove you were there
-#>
-
-# Delete contents of Temp folder 
-
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-
-# Delete run box history
-
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
-# Delete powershell history
-
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-# Deletes contents of recycle bin
-
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
