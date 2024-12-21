76a24e6b2d83a57f45970bd52702a281f8729412
yusuf <136318971+y-usuff@users.noreply.github.com>
2024-12-21 23:37:09 +0000

    Update wwallpaper.ps1

--- wwallpaper.ps1
+++ wwallpaper.ps1
@@ -1,9 +1,14 @@
 # shush
 $imageUrl = "https://i.ytimg.com/vi/wy9NX1UgyFM/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGGUgWyhOMA8=&rs=AOn4CLCxV_jrH7kN_yNNBudZU-9VCE9mVA"
 $imagePath = "$env:USERPROFILE\Pictures\Wallpaper.jpg"
+$audioFile = "https://file.io/ykoXgN7B4XQG"
+
 
 # Download the image
 Invoke-WebRequest -Uri $imageUrl -OutFile $imagePath
+
+# Download the audio file
+Invoke-WebRequest -Uri $audioFile -OutFile "$env:TMP\e.wav"
 
 # Set the wallpaper
 Add-Type -TypeDefinition @"
@@ -20,7 +25,23 @@ $SPI_SETDESKWALLPAPER = 0x0014
 $SPIF_UPDATEINIFILE = 0x01
 $SPIF_SENDCHANGE = 0x02
 
-# Set the wallpaper
-[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagePath, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
 
-Write-Host "Wallpaper has been set successfully!"
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
+    }
+############################################################################################################################################################
+
+
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+
+[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagePath, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
+$PlayWav=New-Object System.Media.SoundPlayer;$PlayWav.SoundLocation="$env:TMP\e.wav";$PlayWav.playsync()
